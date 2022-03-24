bc4b706c2a73c1c52f8dee8d4b71304eb35fbb73
marsel.hakimov@mail.ru <413345Marsel>
2022-03-24 13:13:27 +0300

    added a user greeting

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from aiogram import Bot,types
+import aiogram.utils.markdown as fmt
+from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
@@ -7,18 +8,21 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def bot_start_command(message: types.Message):
-    await message.reply("Привет, давай общаться!)")
+    await message.reply("Привет, назови свое имя!)")
+
 
 @dp.message_handler(commands=['help'])
 async def bot_help_command(message: types.Message):
     await message.reply("Этот бот будет повторять твои сообщения.")
 
+
 @dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id,msg.text)
+async def greeting_command(message: types.Message):
+    await message.answer(fmt.text("Приятно познакомиться: ", fmt.hbold(message.text)), parse_mode=types.ParseMode.HTML)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-
