bbefc91e214c0c89627b7ee5f7b217048eefffbe
marsel.hakimov@mail.ru <413345Marsel>
2022-03-24 13:34:45 +0300

    repeating the user's sticker

--- bot.py
+++ bot.py
@@ -16,12 +16,22 @@ async def bot_start_command(message: types.Message):
 
 @dp.message_handler(commands=['help'])
 async def bot_help_command(message: types.Message):
-    await message.reply("Этот бот будет повторять твои сообщения.")
+    await message.reply("Этот бот будет отвечать на твои сообщения.")
 
 
 @dp.message_handler()
 async def greeting_command(message: types.Message):
     await message.answer(fmt.text("Приятно познакомиться: ", fmt.hbold(message.text)), parse_mode=types.ParseMode.HTML)
+
+
+@dp.message_handler()
+async def download_doc(message: types.Message):
+    await message.document.download()
+
+
+@dp.message_handler(content_types=[types.ContentType.STICKER])
+async def random_sticker(message: types.Message):
+    await message.reply_sticker(message.sticker.file_id)
 
 
 if __name__ == '__main__':
